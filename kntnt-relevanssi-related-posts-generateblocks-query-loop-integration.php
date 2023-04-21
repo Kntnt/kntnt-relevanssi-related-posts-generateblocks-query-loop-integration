928aa91c15323019ca293bc6e776d7aa1eb5ddb2
Thomas Barregren <thomas@kntnt.com>
2023-04-21 15:22:58 +0200

    Create kntnt-relevanssi-related-posts-generateblocks-query-loop-integration.php

---
+++ kntnt-relevanssi-related-posts-generateblocks-query-loop-integration.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       Kntnt Relevanssi Related Posts and GenerateBlocks Query Loop Integration.
+ * Plugin URI:        https://github.com/Kntnt/kntnt-relevanssi-related-posts-generateblocks-query-loop-integration.php
+ * Description:       Replaces the query configured in GenerateBlocks Query Loop with a query returning related posts from Relevanssi if the block wrapped by GenerateBlocks Query Loop (e.g. GenerateBlocks Grid) has the class `relevanssi-related-posts`.
+ * Version:           1.0.0
+ * Author:            Thomas Barregren
+ * Author URI:        https://www.kntnt.com/
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ */
+
+
+defined( 'ABSPATH' ) || die;
+
+add_filter( 'generateblocks_query_loop_args', function ( $query_args, $attributes, $block ) {
+	global $post;
+	if ( isset( $post->ID ) && in_array( 'relevanssi-related-posts', explode( ' ', $block->attributes['className'] ?? '' ) ) && function_exists( 'relevanssi_get_related_post_ids' ) ) {
+		$query_args = [
+			'post__in' => relevanssi_get_related_post_ids( $post->ID ),
+    ];
+	}
+	return $query_args;
+}, 10, 3 );
