c016d9df2c638d5ea5362a81e344aee20773f088
Thomas Barregren <thomas@kntnt.com>
2025-07-11 19:36:40 +0200

    Rewrite to make the logic easier to understand.

--- kntnt-relevanssi-related-posts-generateblocks-query-loop-integration.php
+++ kntnt-relevanssi-related-posts-generateblocks-query-loop-integration.php
@@ -5,7 +5,7 @@
  * Plugin Name:       Kntnt Relevanssi Related Posts and GenerateBlocks Query Loop Integration.
  * Plugin URI:        https://github.com/Kntnt/kntnt-relevanssi-related-posts-generateblocks-query-loop-integration.php
  * Description:       Replaces the query configured in GenerateBlocks Query Loop with a query returning related posts from Relevanssi if the block wrapped by GenerateBlocks Query Loop (e.g. GenerateBlocks Grid) has the class `relevanssi-related-posts`.
- * Version:           1.2.0
+ * Version:           1.2.1
  * Author:            Thomas Barregren
  * Author URI:        https://www.kntnt.com/
  * License:           GPL-3.0+
@@ -16,8 +16,33 @@
 defined( 'ABSPATH' ) || die;
 
 add_filter( 'generateblocks_query_loop_args', function ( $query_args, $attributes, $block ) {
+
+	// Abort if Relevanssi Pro is not installed.
+	if ( ! function_exists( 'relevanssi_get_related_post_ids' ) ) {
+		return $query_args;
+	}
+
+	// Abort if the block doen't have the magic lass `relevanssi-related-posts`.
+	$has_magic_class = in_array( 'relevanssi-related-posts', explode( ' ', $block->attributes['className'] ?? '' ) );
+	if ( ! $has_magic_class ) {
+		return $query_args;
+	}
+
+	/**
+	 * Filter that allows external to contrl whether or not to proceed with the integration.
+	 *
+	 * @param bool  $proceed    Whether to proceed with the integration.
+	 * @param array $query_args The query arguments.
+	 *
+	 * @return bool Whether to proceed with the integration.
+	 */
+	$proceed = apply_filters( 'kntnt-relevanssi-related-posts-generateblocks-query-loop-integration', true, $query_args );
+	if ( ! $proceed ) {
+		return $query_args;
+	}
+
 	global $post;
-	if ( function_exists( 'relevanssi_get_related_post_ids' ) && isset( $post->ID ) & in_array( 'relevanssi-related-posts', explode( ' ', $block->attributes['className'] ?? '' ) ) && apply_filters( 'kntnt-relevanssi-related-posts-generateblocks-query-loop-integration', true, $query_args ) ) {
+	if ( isset( $post->ID ) && $post->ID ) {
 		$query_args = [
 			'post__in' => relevanssi_get_related_post_ids( $post->ID ),
 			'orderby' => 'post__in',
@@ -25,5 +50,7 @@ add_filter( 'generateblocks_query_loop_args', function ( $query_args, $attribute
 			'ignore_sticky_posts' => 1,
 		];
 	}
+
 	return $query_args;
+
 }, 10, 3 );
